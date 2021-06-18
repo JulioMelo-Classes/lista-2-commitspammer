2d56dc74a55e8e8bc06bf235a83eb330c981251e
commitspammer <gustavo.ferreira.110@ufrn.edu.br>
2021-06-18 16:53:24 -0300

    Questao 'copy' finalizada.

--- copy/include/copy.h
+++ copy/include/copy.h
@@ -10,15 +10,26 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * TODO: documentação no estilo doxygen
+/*!
+ * @brief Copies the elements from a [first,last) range to a new range
+ * starting at d_first.
+ * @tparam InputIt Iterator type.
+ * @param first Iterator pointing to range's first element.
+ * @param last Iterator pointing to position after range's last element.
+ * @return Iterator pointing to position after the new range's last
+ * element.
  */
 template<class InputIt>
 InputIt copy(InputIt first, InputIt last, InputIt d_first)
 {
-    // TODO
-    return first;
+	while (first != last) {
+		*d_first = *first;
+		++first;
+		++d_first;
+	}
+	return d_first + 1;
 }
 
 }
 #endif
+
