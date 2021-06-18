16c0d32afaddc1c4035c991e64f009c406e7e335
commitspammer <gustavo.ferreira.110@ufrn.edu.br>
2021-06-18 16:57:13 -0300

    Questao 'find_if' finalizada.

--- find_if/include/find_if.h
+++ find_if/include/find_if.h
@@ -10,15 +10,28 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * TODO: documentação no estilo doxygen
+/*!
+ * @brief Searches for first element in [first,last) range to meet
+ * predicate p.
+ * @tparam InputIt Iterator type.
+ * @tparam UnaryPredicate Predicate type for "bool p(const T& a)".
+ * @param first Iterator pointing to range's first element.
+ * @param last Iterator pointing to position after range's last element.
+ * @param p Function object used for checking condition.
+ * @return Iterator pointing to first element found, or to last if no
+ * element was found.
  */
 template<class InputIt, class UnaryPredicate>
 InputIt find_if(InputIt first, InputIt last, UnaryPredicate p)
 {
-    // TODO
-    return first;
+	while (first != last) {
+		if (p(*first))
+			return first;
+		++first;
+	}
+	return last;
 }
 
 }
 #endif
+
