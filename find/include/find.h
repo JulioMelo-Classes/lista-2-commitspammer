7a0f2f174e68664bc4f238e327e93565c65dfa6f
commitspammer <gustavo.ferreira.110@ufrn.edu.br>
2021-06-18 17:00:15 -0300

    Questao 'find' finalizada.

--- find/include/find.h
+++ find/include/find.h
@@ -10,15 +10,30 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * TODO: documentação no estilo doxygen
+/*!
+ * @brief Searches for first element in [first,last) range to be equal
+ * to value.
+ * @tparam InputIt Iterator type.
+ * @tparam T Iterated type.
+ * @tparam Equal Function obj type for "bool eq(const T& a, const T& B)".
+ * @param first Iterator pointing to range's first element.
+ * @param last Iterator pointing to position after range's last element.
+ * @param value Value to be compared to.
+ * @param eq Function object used for comparing values.
+ * @return Iterator pointing to first element found, or to last if no
+ * element was found.
  */
 template<class InputIt, class T, class Equal>
 InputIt find(InputIt first, InputIt last, const T& value, Equal eq)
 {
-    // TODO
-    return first;
+	while (first != last) {
+		if (eq(*first, value))
+			return first;
+		++first;
+	}
+	return last;
 }
 
 }
 #endif
+
