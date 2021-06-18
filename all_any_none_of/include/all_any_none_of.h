78260777700bc28978b76a31676250ab05208eeb
commitspammer <gustavo.ferreira.110@ufrn.edu.br>
2021-06-18 17:01:15 -0300

    Questao 'all_any_none_of' finalizada.

--- all_any_none_of/include/all_any_none_of.h
+++ all_any_none_of/include/all_any_none_of.h
@@ -10,35 +10,68 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * TODO: documentação no estilo doxygen
+/*!
+ * @brief Tells if all elements in [first,last) range meet predicate p.
+ * @tparam InputIt Iterator type.
+ * @tparam UnaryPredicate Function obj type for "bool p(const T& a)".
+ * @param first Iterator pointing to range's first element.
+ * @param last Iterator pointing to position after range's last element.
+ * @param p Function object used for checking condition.
+ * @return true if all elements meet the condition, or false if not.
  */
 template<class InputIt, class UnaryPredicate>
 bool all_of(InputIt first, InputIt last, UnaryPredicate p)
 {
-    // TODO
-    return false;
+	if (first == last)
+		return false;
+	while (first != last) {
+		if (!p(*first))
+			return false;
+		++first;
+	}
+	return true;
 }
 
-/*! 
- * TODO: documentação no estilo doxygen
+/*!
+ * @brief Tells if any element in [first,last) range meets predicate p.
+ * @tparam InputIt Iterator type.
+ * @tparam UnaryPredicate Function obj type for "bool p(const T& a)".
+ * @param first Iterator pointing to range's first element.
+ * @param last Iterator pointing to position after range's last element.
+ * @param p Function object used for checking condition.
+ * @return true if any element meets the condition, or false if not.
  */
 template<class InputIt, class UnaryPredicate>
 bool any_of(InputIt first, InputIt last, UnaryPredicate p)
 {
-    // TODO
-    return false;
+	while (first != last) {
+		if (p(*first))
+			return true;
+		++first;
+	}
+	return false;
 }
 
-/*! 
- * TODO: documentação no estilo doxygen
+/*!
+ * @brief Tells if no element in [first,last) range meets predicate p.
+ * @tparam InputIt Iterator type.
+ * @tparam UnaryPredicate Function obj type for "bool p(const T& a)".
+ * @param first Iterator pointing to range's first element.
+ * @param last Iterator pointing to position after range's last element.
+ * @param p Function object used for checking condition.
+ * @return true if no element meets the condition, or false if any does.
  */
 template<class InputIt, class UnaryPredicate>
 bool none_of(InputIt first, InputIt last, UnaryPredicate p)
 {
-    // TODO
-    return false;
+	while (first != last) {
+		if (p(*first))
+			return false;
+		++first;
+	}
+	return true;
 }
 
 }
 #endif
+
