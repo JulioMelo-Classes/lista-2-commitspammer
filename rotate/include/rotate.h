0acc82399d09359abd50b9e121a7974ab4d0ac63
commitspammer <gustavo.ferreira.110@ufrn.edu.br>
2021-06-20 15:08:39 -0300

    Questao 'rotate' finalizada.

--- rotate/include/rotate.h
+++ rotate/include/rotate.h
@@ -10,15 +10,34 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * TODO: documentação no estilo doxygen
+/*!
+ * @brief Rotates elements in [first,last) range until n_first becomes
+ * first element.
+ * @tparam ForwardIt Iterator type.
+ * @param first Iterator pointing to range's first element.
+ * @param n_first Iterator pointing to element to be put as first.
+ * @param last Iterator pointing to position after range's last element.
+ * @return Iterator pointing to original first element.
  */
 template<class ForwardIt>
 ForwardIt rotate(ForwardIt first, ForwardIt n_first, ForwardIt last)
 {
-    // TODO
-   return last;
+	if (first == n_first)
+		return first;
+	ForwardIt og_first {first};
+	for (int i = 0; i < last - n_first + 1; ++i) {
+		ForwardIt tmp {first};
+		while (tmp+1 < last) {
+			std::swap(*tmp, *(tmp+1));
+			++tmp;
+		}
+		if (og_first)
+		++og_first;
+	}
+	//P.S.: This is inefficient lol
+	return og_first;
 }
 
 }
 #endif
+
