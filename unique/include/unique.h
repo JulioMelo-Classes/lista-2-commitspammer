3fb46e82dbf443086e92ca3cd8379b335c1056ee
commitspammer <gustavo.ferreira.110@ufrn.edu.br>
2021-06-20 15:06:50 -0300

    Questao 'unique' finalizada.

--- unique/include/unique.h
+++ unique/include/unique.h
@@ -10,15 +10,37 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * TODO: documentação no estilo doxygen
+/*!
+ * @brief Removes duplicated elements from [first,last) range.
+ * @tparam InputIt Iterator type.
+ * @tparam Equal Function obj type for "bool eq(const T& a, const T& b)".
+ * @param first Iterator pointing to range's first element.
+ * @param last Iterator pointing to position after range's last element.
+ * @param eq Function object used for comparing values.
+ * @return Iterator pointing to position after range's new last element;
  */
 template<class InputIt, class Equal>
-InputIt unique( InputIt first, InputIt last, Equal eq )
+InputIt unique(InputIt first, InputIt last, Equal eq)
 {
-    // TODO
-    return last;
+	while (first != last) {
+		InputIt next {first+1};
+		while (next != last) {
+			if (!eq(*first, *next)) {
+				++next;
+			} else {
+				InputIt tmp {next};
+				while (tmp+1 < last) {
+					std::swap(*tmp, *(tmp+1));
+					++tmp;
+				}
+				--last;
+			}
+		}
+		++first;
+	}
+	return last;
 }
 
 }
 #endif
+
