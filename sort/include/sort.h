b3f7fa90735ebbeb13e96b95fd0def5cf3e43a20
commitspammer <gustavo.ferreira.110@ufrn.edu.br>
2021-06-20 15:08:08 -0300

    Questao 'sort' finalizada.

--- sort/include/sort.h
+++ sort/include/sort.h
@@ -10,14 +10,30 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * TODO: documentação no estilo doxygen
+/*!
+ * @brief Sorts elements inside [first,last) range.
+ * @tparam ForwardIt Iterator type.
+ * @tparam Comparison Function obj type for
+ * "bool eq(const T& a, const T& B)".
+ * @param first Iterator pointing to range's first element.
+ * @param last Iterator pointing to position after range's last element.
+ * @param cmp Function object used for comparing values.
  */
 template<class ForwardIt, class Comparison>
 void sort(ForwardIt first, ForwardIt last, Comparison cmp)
 {
-    // TODO
+	ForwardIt start {first};
+	while (first != last) {
+		ForwardIt first2 {start};
+		while (first2 != last) {
+			if (!cmp(*first2, *(first2+1)))
+				std::swap(*first2, *(first2+1));
+			++first2;
+		}
+		++first;
+	}
 }
 
 }
 #endif
+
