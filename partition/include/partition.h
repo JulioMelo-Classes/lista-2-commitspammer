3e3e062a0d1a47fade220d41c0b01d6924faf7d2
commitspammer <gustavo.ferreira.110@ufrn.edu.br>
2021-06-20 15:07:25 -0300

    Questao 'partition' finalizada.

--- partition/include/partition.h
+++ partition/include/partition.h
@@ -10,15 +10,38 @@ using std::sort;
 
 namespace graal {
 
-/*! 
- * TODO: documentação no estilo doxygen
+/*!
+ * @brief Separates elements in [first,last) range between those that
+ * meet predicate p and those that don't.
+ * @tparam ForwardIt Iterator type.
+ * @tparam UnaryPredicate Function obj type for "bool p(const T& a)".
+ * @param first Iterator pointing to range's first element.
+ * @param last Iterator pointing to position after range's last element.
+ * @param p Function object used for checking condition.
+ * @return Iterator pointing to position after last element that meets
+ * predicate p.
  */
+
 template<class ForwardIt, class UnaryPredicate>
 ForwardIt partition(ForwardIt first, ForwardIt last, UnaryPredicate p)
 {
-    // TODO
-    return last;
+	int count {0};
+	long int max_count {std::distance(first, last)};
+	while (first != last && count < max_count) {
+		if (!p(*first)) {
+			ForwardIt tmp {first};
+			while (tmp+1 < last) {
+				std::swap(*tmp, *(tmp+1));
+				++tmp;
+			}
+			++count;
+		} else {
+			++first;
+		}
+	}
+	return first;
 }
 
 }
 #endif
+
